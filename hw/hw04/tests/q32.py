4d59f4f69cdb22392150a865373e4ab7f078dbdf
susanswang <40613331+susanswang@users.noreply.github.com>
2021-09-18 13:06:15 -0700

    Add files via upload

---
+++ hw/hw04/tests/q32.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q32',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> num_of_Email
+          19
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
