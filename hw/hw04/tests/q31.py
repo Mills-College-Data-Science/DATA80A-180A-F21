10d320c4bc6f20cc774e9e5198e129e4f4edcdb9
susanswang <40613331+susanswang@users.noreply.github.com>
2021-09-18 13:05:56 -0700

    Create q31.py

---
+++ hw/hw04/tests/q31.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q31',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> num_of_breaches
+          128
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
