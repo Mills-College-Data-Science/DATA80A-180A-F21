217e39287d27695457a94c85e3a6c3467cbb496d
susanswang <40613331+susanswang@users.noreply.github.com>
2021-09-24 21:15:09 -0700

    Add files via upload

---
+++ lab/lab05/tests/q11.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'q11',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure your function has the proper syntax!;
+          >>> twenty_percent == to_percentage(.2)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ lab/lab05/tests/q35.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q35',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> num_ceos_more_than_30_million == 5
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
