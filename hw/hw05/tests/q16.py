a7586390de9922bcd1f6de0954a70015b8874b68
susanswang <40613331+susanswang@users.noreply.github.com>
2021-09-25 12:47:41 -0700

    Add files via upload

---
+++ hw/hw05/tests/q16.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'q16',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Number of columns of the resulting table should be 3;
+          >>> best_restaurant 
+          'Mikes Taco Club'
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ hw/hw05/tests/q24.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'q24',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> avg_prof_salary 
+          199948.77093023257
+          """,
+          'hidden': False,
+          'locked': False
+        }
+        
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
