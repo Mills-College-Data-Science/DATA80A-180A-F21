7743b89f427422a5e6d6b72755ed286efeb91235
susanswang <40613331+susanswang@users.noreply.github.com>
2021-10-25 11:23:53 -0700

    Add files via upload

---
+++ proj/project2/parts1-2/tests/q2_1_1.py
@@ -0,0 +1,4 @@
+test = {   'name': 'q2_1_1',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> abs(observed_diabetes_distance - 0.018029) < 1e-6\nTrue', 'hidden': False, 'locked': False}], 
+                'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ proj/project2/parts1-2/tests/q2_1_6.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_1_6',
+    'points': [0],
+    'suites': [   {   'cases': [{'code': '>>> sorted(framingham_explanations) == make_array(1,2,3)\narray([ True,  True,  True])', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
