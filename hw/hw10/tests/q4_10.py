da183588cb90f5e530f4526db9567245d2ed29c8
susanswang <40613331+susanswang@users.noreply.github.com>
2021-11-06 22:20:28 -0700

    Add files via upload

---
+++ hw/hw10/tests/q4_10.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q4_10',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 > empirical_P > 0.05
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ hw/hw10/tests/q4_6.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'q4_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(observed_difference, float)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> round(observed_difference, 3)
+          0.607
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
