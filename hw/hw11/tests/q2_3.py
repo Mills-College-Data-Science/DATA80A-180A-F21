91db7330cbdf290f2c8d45f9ccdc8eccf5192228
susanswang <40613331+susanswang@users.noreply.github.com>
2021-11-13 22:36:06 -0800

    Add files via upload

---
+++ hw/hw11/tests/q2_3.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_3',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> 750 <= smallest_num <= 760\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ hw/hw11/tests/q3_9.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q3_9',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> min_sufficient == True \nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
