2184f5b1d4f2fdddd10a31eb5c9129a0cc74638c
susanswang <40613331+susanswang@users.noreply.github.com>
2021-11-27 19:17:29 -0800

    Add files via upload

---
+++ hw/hw12/tests/q3_4.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q3_4',
+    'points': [0, 0],
+    'suites': [   {   'cases': [{'code': '>>> 0.4 <= spread_slope < 1\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> spread_intercept > 0\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
